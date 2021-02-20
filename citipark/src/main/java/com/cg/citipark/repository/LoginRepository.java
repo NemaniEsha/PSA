796dea20534ef0c419bb735be1236ded21793d8b
NemaniEsha <76994003+NemaniEsha@users.noreply.github.com>
2021-02-20 11:45:00 +0530

    Update LoginRepository.java

--- citipark/src/main/java/com/cg/citipark/repository/LoginRepository.java
+++ citipark/src/main/java/com/cg/citipark/repository/LoginRepository.java
@@ -1,10 +1,1 @@
-package com.cg.citipark.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.citipark.beans.Login;
-
-
-public interface LoginRepository extends JpaRepository<Login,String>{
-
-}
